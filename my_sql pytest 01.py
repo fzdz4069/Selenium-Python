b8f727409846298589fce0aee797b6960281ec7b
fzdz4069 <99118483+fzdz4069@users.noreply.github.com>
2022-04-26 13:30:14 +0200

    Add files via upload

---
+++ my_sql pytest 01.py
@@ -0,0 +1,19 @@
+# Testcase 1: A basic test of chromedriver and of the function call to record test results.
+
+from record_test import record_test
+import pytest
+from selenium import webdriver
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://live.techpanda.org/index.php/")
+driver.maximize_window()
+title = driver.title
+expected_title = "Home page"
+
+condition = title == expected_title
+record_test("1", "techpanda.org", "Testing function call", condition)
+
+driver.close()
---
+++ my_sql pytest 02.py
@@ -0,0 +1,22 @@
+# Testcase 2: Testing page title and recording the test result in the database.
+
+from record_test import record_test
+import pytest
+from selenium import webdriver
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.donlope.net/fz/lyrics/")
+driver.maximize_window()
+
+title = driver.title
+expected_title = "FZ Discography"
+assert title.lower() == expected_title.lower()
+print(title)
+
+condition = title.lower() == expected_title.lower()
+record_test("2", "donlope.net/fz/lyrics", "Testing page title", condition)
+
+driver.close()
