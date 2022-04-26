8e3bbf7cc2080d08fe418d93492a94ac88b5e845
fzdz4069 <99118483+fzdz4069@users.noreply.github.com>
2022-04-26 15:47:14 +0200

    Update record_test.py

--- record_test.py
+++ record_test.py
@@ -10,8 +10,8 @@ def record_test(test_id, website, testcase, condition):
         passwd="abcde12345",
         database="python")
     cursor = mydb.cursor()
-    insert = "insert into pytest(website, testcase) values (%s, %s)"
-    test_n = (website, testcase)
+    insert = "insert into pytest(test_id, website, testcase) values (%s, %s, %s)"
+    test_n = (test_id, website, testcase)
     query_t = "update pytest set result='passed' where test_id=" + test_id
     query_f = "update pytest set result='failed' where test_id=" + test_id
     cursor.execute(insert, test_n)
