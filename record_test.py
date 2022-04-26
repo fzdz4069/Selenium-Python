a65917a526d10c08f64951122193f952f7057caf
fzdz4069 <99118483+fzdz4069@users.noreply.github.com>
2022-04-26 13:02:26 +0200

    Add files via upload

---
+++ record_test.py
@@ -0,0 +1,32 @@
+# A function created to be called for recording test results in the MySQL database.
+
+import mysql.connector
+
+
+def record_test(test_id, website, testcase, condition):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="abcde12345",
+        database="python")
+    cursor = mydb.cursor()
+    insert = "insert into pytest(website, testcase) values (%s, %s)"
+    test_n = (website, testcase)
+    query_t = "update pytest set result='passed' where test_id=" + test_id
+    query_f = "update pytest set result='failed' where test_id=" + test_id
+    cursor.execute(insert, test_n)
+
+    if condition:
+        cursor.execute(query_t)
+    else:
+        cursor.execute(query_f)
+
+    query = "select * from pytest"
+    cursor.execute(query)
+
+    for col in cursor:
+        print(col)
+
+    mydb.commit()
+    cursor.close()
+    mydb.close()
