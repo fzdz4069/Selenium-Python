5415ef1dacf1b90bcfd6f5edc2ea558fabb67408
fzdz4069 <99118483+fzdz4069@users.noreply.github.com>
2022-04-26 15:44:50 +0200

    Update my_sql pytest 00.py

--- my_sql pytest 00.py
+++ my_sql pytest 00.py
@@ -17,15 +17,15 @@ cursor.execute("CREATE table pytest ("
                "result varchar(10));")
 
 insert = "insert into pytest(test_id, website, testcase) values (%s, %s, %s)"
-test1 = (1, 'database test', 'Testing connection')
+test1 = (0, 'database test', 'Testing connection')
 query = "select * from pytest"
-query_t = "update pytest set result='passed' where test_id=1"
-query_f = "update pytest set result='failed' where test_id=1"
+query_t = "update pytest set result='passed' where test_id=0"
+query_f = "update pytest set result='failed' where test_id=0"
 cursor.execute(insert, test1)
 cursor.execute(query)
 
 for col in cursor:
-    if col[0] == 1:
+    if col[0] == 0:
         cursor.execute(query_t)
     else:
         cursor.execute(query_f)
